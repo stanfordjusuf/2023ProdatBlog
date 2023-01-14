c0698527db8d57a9e5e83fdfd917a7cc0f848e8a
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2023-01-14 17:49:59 +0700

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer footer-center p-10 bg-primary text-primary-content">
+    <img width="50px" height="50px" src="https://i.ibb.co/tbJQrh0/logo.png" alt="logo" border="0" />
+    <p className="text-white font-bold">
+      Prodat <br />
+      Est 2022
+    </p>
+    <p className="text-white">Copyright © 2022 - All right reserved</p>
+  </footer>
+);
+
+export default Footer;
---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import '../styles/globals.scss';
+import { Layout } from '../components';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Analytics />
+    </Layout>
+  );
+}
+
+export default MyApp;
